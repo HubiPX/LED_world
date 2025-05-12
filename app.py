833ca38a46ff3b9ee89be9c4da5cb10d6c99c4fe
Hubert Mentel <hubert.mentel1@gmail.com>
2025-05-12 10:55:00 +0200

    add

--- app.py
+++ app.py
@@ -22,4 +22,4 @@ def index():
     return open('index.html').read()  # Ładuje stronę HTML
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080)  # Gunicorn uruchomi aplikację, więc ta linia jest niekonieczna
