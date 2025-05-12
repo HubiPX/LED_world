e39264cf9c7ebb24562dd767b1cdd9a1e41527ff
Hubert Mentel <hubert.mentel1@gmail.com>
2025-05-12 12:23:13 +0200

    info o zwarciu na stronie

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import Flask, request
 app = Flask(__name__)
 
 led_state = "OFF"  # Stan diody
+short_count = 0  # Licznik zwarć
+
 
 @app.route('/set')
 def set_led():
@@ -13,13 +15,32 @@ def set_led():
         return f"LED ustawiono na: {led_state}"
     return "Nieprawidłowa wartość"
 
+
 @app.route('/get')
 def get_led():
     return led_state
 
+
+@app.route('/set_count')
+def set_count():
+    global short_count
+    count = request.args.get('count')
+
+    if count is not None and count.isdigit():
+        short_count = int(count)  # Zapisz liczbę zwarć
+        return f"Zliczono {short_count} zwarć."
+    return "Nieprawidłowa wartość"
+
+
+@app.route('/get_count')
+def get_count():
+    return str(short_count)
+
+
 @app.route('/')
 def index():
     return open('index.html').read()  # Ładuje stronę HTML
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)  # Gunicorn uruchomi aplikację, więc ta linia jest niekonieczna
