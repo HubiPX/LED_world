1bc3bfce5e21759366463f7a33295d8b6a65ba22
Hubert Mentel <hubert.mentel1@gmail.com>
2025-05-18 21:43:57 +0200

    gate

--- app.py
+++ app.py
@@ -2,45 +2,27 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-led_state = "OFF"  # Stan diody
-short_count = 0  # Licznik zwarć
+hall_state = "closed"
+click_count = 0
 
-
-@app.route('/set')
-def set_led():
-    global led_state
-    state = request.args.get('led')
-    if state in ["ON", "OFF"]:
-        led_state = state
-        return f"LED ustawiono na: {led_state}"
-    return "Nieprawidłowa wartość"
-
+@app.route('/set_status')
+def set_status():
+    global hall_state
+    state = request.args.get("state")
+    if state in ["open", "closed"]:
+        hall_state = state
+        return f"Status ustawiony na: {state}"
+    return "Błąd", 400
 
 @app.route('/get')
-def get_led():
-    return led_state
+def get_relay_command():
+    return f"{click_count},{hall_state}"
 
-
-@app.route('/set_count')
-def set_count():
-    global short_count
-    count = request.args.get('count')
-
-    if count is not None and count.isdigit():
-        short_count = int(count)  # Zapisz liczbę zwarć
-        return f"Zliczono {short_count} zwarć."
-    return "Nieprawidłowa wartość"
-
-
-@app.route('/get_count')
-def get_count():
-    return str(short_count)
-
-
-@app.route('/')
-def index():
-    return open('index.html').read()  # Ładuje stronę HTML
-
+@app.route('/click')
+def register_click():
+    global click_count
+    click_count += 1
+    return f"Kliknięcie zarejestrowane. Nowa wartość: {click_count}"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)  # Gunicorn uruchomi aplikację, więc ta linia jest niekonieczna
+    app.run(host='0.0.0.0', port=8080)
