29e4e84561222a4f648d94e49d5076b951517482
Hubert Mentel <hubert.mentel1@gmail.com>
2025-05-18 22:06:24 +0200

    gate

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
+import os
 from flask import Flask, request
 
 app = Flask(__name__)
 
 hall_state = "closed"
-click_count = 0
+click_count = None  # Flaga jednorazowa
 
 
 @app.route('/')
 def index():
     return open('index.html').read()
+
 
 @app.route('/set_status')
 def set_status():
@@ -19,15 +21,24 @@ def set_status():
         return f"Status ustawiony na: {state}"
     return "Błąd", 400
 
+
 @app.route('/get')
 def get_relay_command():
-    return f"{click_count},{hall_state}"
+    global click_count
+    # Pobieramy aktualny stan
+    response = f"{click_count if click_count is not None else 0},{hall_state}"
+    # Zerujemy po odczycie
+    click_count = None
+    return response
+
 
 @app.route('/click')
 def register_click():
     global click_count
-    click_count += 1
-    return f"Kliknięcie zarejestrowane. Nowa wartość: {click_count}"
+    click_count = 1
+    return "Kliknięcie zarejestrowane."
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
