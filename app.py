f644d82d0ca72f7f55034c974379ba1ede6d0b38
Hubert Mentel <hubert.mentel1@gmail.com>
2025-05-18 21:48:17 +0200

    gate

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 hall_state = "closed"
 click_count = 0
+
+
+@app.route('/')
+def home():
+    return render_template("index.html")
 
 @app.route('/set_status')
 def set_status():
