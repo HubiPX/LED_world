5e830a5d5b876815debc2c977731ccc85925a557
Hubert Mentel <hubert.mentel1@gmail.com>
2025-05-21 17:14:26 +0200

    login page

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 import os
 import time
-from flask import Flask, request
+from flask import Flask, request, redirect, session, url_for
+from werkzeug.security import check_password_hash
 
 app = Flask(__name__)
+
+# Sekretna wartość do sesji
+app.secret_key = os.getenv("FLASK_SECRET_KEY")
+
+# Oczekiwany hasz hasła użytkownika
+PASSWORD_HASH = os.getenv("PASSWORD_HASH")
 
 hall_state = "closed"
 click = 0
@@ -11,7 +18,27 @@ click_time = 0
 
 @app.route('/')
 def index():
+    if not session.get("logged_in"):
+        return redirect(url_for("login"))
     return open('index.html').read()
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        password = request.form.get('password')
+        if check_password_hash(PASSWORD_HASH, password):
+            session['logged_in'] = True
+            return redirect(url_for('index'))
+        else:
+            return "Błędne hasło", 401
+    return open('login.html').read()
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('login'))
 
 
 @app.route('/set_status')
@@ -23,16 +50,19 @@ def set_status():
         return f"Status ustawiony na: {state}"
     return "Błąd", 400
 
+
 @app.route('/get_status')
 def get_status():
+    if not session.get("logged_in"):
+        return "Nieautoryzowany", 401
     response = f"{hall_state}"
     return response
+
 
 @app.route('/get')
 def get_relay_command():
     global click, click_time
 
-    # Sprawdzenie, czy minęło 60 sekund od kliknięcia
     if click == 1 and time.time() - click_time > 60:
         click = 0
 
@@ -46,6 +76,9 @@ def get_relay_command():
 
 @app.route('/click')
 def register_click():
+    if not session.get("logged_in"):
+        return "Nieautoryzowany", 401
+
     global click, click_time
     click = 1
     click_time = time.time()
